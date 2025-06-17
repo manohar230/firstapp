11675ce426441d3fd90ef6f2eb66ee28bd498e44
Purella Manohar Sri venkat <manoharvenkat14@gmail.com>
2025-06-17 15:19:19 +0530

    flask install

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("hello world")
+from flask import Flask,render_template,request,redirect
+from flask_mysqldb import MySQL
+from datetime import datetime
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] =''
+app.config['MYSQL_DB'] ="example"
+
+mysql = MySQL(app)
+@app.route('/')
+def index():
+    return render_template("index.html")
